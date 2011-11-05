a2ab6fef8eb2dd35306ebea0f7a568ebf568440d
John Tolson <jtolson@mytechconcepts.com>
2011-11-05 09:51:47 -0400

    Mini Work Model

---
+++ test/unit/com/tci/utilities/ExcelBuilderTest.groovy
@@ -0,0 +1,32 @@
+package com.tci.utilities;
+
+import com.tci.utilities.*
+import org.junit.After
+import org.junit.Before
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jtolson
+ * Date: 11/5/11
+ * Time: 9:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExcelBuilderTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testDDL()
+    {
+        ExcelBuilder  a = new ExcelBuilder("/Users/jtolson/ExampleFile.xls")
+
+        def t = new TableDDLBuilder("/Users/jtolson/ExampleFile.xls")
+        t.getSpreadSheetValues()
+    }
+}
